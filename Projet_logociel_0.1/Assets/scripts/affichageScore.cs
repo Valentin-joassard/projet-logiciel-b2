951524fbd25c9acf798ee948429150649cd312d5
Maxence <maxence.crosse@ynov.com>
2021-05-27 11:51:58 +0200

    affichage score fait

---
+++ Projet_logociel_0.1/Assets/scripts/affichageScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class affichageScore : MonoBehaviour
+{
+    public Text score;
+    public GameObject joueur;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score.text = joueur.GetComponent<StreakScore>().GetScore().ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = joueur.GetComponent<StreakScore>().GetScore().ToString();
+    }
+}
